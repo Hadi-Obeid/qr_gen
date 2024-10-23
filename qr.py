76d1ae659ce28d573ff85372589fb3534887ecca
Hadi_Obeid24 <hobeid1212@gmail.com>
2024-10-23 09:23:40 +0300

    implementing template of qr coding

--- qr.py
+++ qr.py
@@ -9,6 +9,40 @@ class QREncoding(Enum):
     KANJI = 0b1000
     ECI = 0b0111
 
+alphanumeric = {x:i for i, x in enumerate([*"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:"])}
+print(alphanumeric)
+
+def get_char_count_bits(version, encoding):
+    if version < 1 or version > 40:
+        raise ValueError("ERROR: QR Version must be between 1 and 40!")
+    match encoding:
+        case QREncoding.NUMERIC:
+            if version < 10:
+                return 10
+            elif version < 27:
+                return 12
+            else:
+                return 14
+        case QREncoding.ALPHA:
+            if version < 10:
+                return 9
+            elif version < 27:
+                return 11
+            else:
+                return 13
+        case QREncoding.BYTE:
+            return 8 if version < 10 else 16
+        case QREncoding.KANJI:
+            if version < 10:
+                return 8
+            elif version < 27:
+                return 10
+            else:
+                return 12
+
 class QRCode:
-    def __init__(self):
-        self.encoding = QREncoding.ALPHA
+    def __init__(self, encoding = QREncoding.ALPHA, quality = "Q", version = 1):
+        self.encoding = encoding
+        self.quality = quality
+        self.version = version
+        self.char_count_bits = get_char_count_bits(self.version, self.encoding)
